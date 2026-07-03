2d056b01b84c82ca3bbde04bb74ad2e748399479
Prajwal Kalode <prajwalkalode76@gmail.com>
2026-07-03 17:36:27 +0530

    Removed swagger apis from code

--- services/analytics-service/app.js
+++ services/analytics-service/app.js
@@ -1,14 +1,11 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
 import analyticsRouter from './router/analyticsRouter.js';
 import healthRouter from './router/healthRouter.js';
-import swaggerSpec from './swagger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/analytics', analyticsRouter);
 app.use('/', healthRouter);
 
